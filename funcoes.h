dadffe7fd827f35520eb541e78c4b77f89d2d0d8
octaviioo <octaviopatrocinio@hotmail.com>
2024-12-02 14:38:31 -0300

    Atualizacoes

---
+++ funcoes.h
@@ -0,0 +1,155 @@
+// Define os códigos de cores ANSI
+#define RESET "\033[0m"
+#define CYAN "\033[34m"
+#define RED "\033[31m"
+
+typedef struct Navio {
+    int x;
+    int y;
+    int direcao;  
+    int valida;   
+    int tamanho; 
+} Navio;
+
+void draw_box_with_text(int width, int height, const char *text) {
+    int text_row = height / 2;               
+    int text_col = (width - strlen(text)) / 2; 
+    
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (i == 0 || i == height - 1) {
+                printf(CYAN "*"); 
+            } else if (j == 0 || j == width - 1) {
+                printf(CYAN "*"); 
+            } else if (i == text_row && j >= text_col && j < text_col + strlen(text)) {
+                printf(RESET "%c", text[j - text_col]); 
+            } else {
+                printf(" ");
+            }
+        }
+        printf(RESET "\n"); 
+    }
+}
+
+void zeraMatriz(int matriz[7][7]) {
+    for (int i = 0; i < 7; i++) {
+        for (int j = 0; j < 7; j++) {
+            matriz[i][j] = 0;
+        }
+    }
+}
+
+void imprimeMatriz(int matriz[7][7], int tipo){
+
+    if(!tipo){
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                printf("%d ", matriz[i][j]);
+            }
+            printf("\n");
+        }
+    }else{
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                switch (matriz[i][j])
+                {
+                case 1:
+                    printf(CYAN"%d ", matriz[i][j]);
+                    break;
+
+                case 2:
+                    printf(RED"%d ", matriz[i][j]);
+                    break;
+                
+                default:
+                    printf(RESET"%d ", matriz[i][j]);
+                    break;
+                }
+                
+                
+            }
+            printf("\n");
+        }
+    }
+
+    
+}
+
+int verificaPosicaoValida(int tabuleiro[7][7], Navio *navio) {
+    if (navio->direcao == 0) {  
+        if (navio->y + navio->tamanho > 7) {  
+            return 0;
+        }
+        for (int i = 0; i < navio->tamanho; i++) {
+            if (tabuleiro[navio->x][navio->y + i] != 0) { 
+                return 0;
+            }
+        }
+    } else {
+        if (navio->x + navio->tamanho > 7) {  
+            return 0;
+        }
+        for (int i = 0; i < navio->tamanho; i++) {
+            if (tabuleiro[navio->x + i][navio->y] != 0) {  
+            }
+        }
+    }
+    return 1;  
+}
+
+void geraNavio(int tabuleiro[7][7], Navio *navio) {
+
+    navio->valida = 0;
+
+    while (!navio->valida) {
+        navio->x = rand() % 7; 
+        navio->y = rand() % 7;   
+        navio->direcao = rand() % 2; 
+        navio->valida = 0;  
+
+        if (verificaPosicaoValida(tabuleiro, navio)) {
+            navio->valida = 1;
+           
+            for (int i = 0; i < navio->tamanho; i++) {
+                if (navio->direcao == 0) { 
+                    tabuleiro[navio->x][navio->y + i] = 1;
+                } else {  
+                    tabuleiro[navio->x + i][navio->y] = 1;
+                }
+            }
+        }
+    }
+}
+
+void fazJogada(int jogada[7][7], int tabuleiro[7][7])
+{
+
+    int cooJogada[2], validaJogada;
+    validaJogada = 1;
+
+    /*
+    Valor 0 -> inexplorado
+    Valor 1 -> Agua
+    Valor 2 -> Barco
+    */
+
+    while (validaJogada)
+    {
+
+        cooJogada[0] = rand() % 7;
+        cooJogada[1] = rand() % 7;
+
+        if (jogada[cooJogada[0]][cooJogada[1]] == 0)
+        {
+            if (tabuleiro[cooJogada[0]][cooJogada[1]])
+            {
+                jogada[cooJogada[0]][cooJogada[1]] = 2;
+            }
+            else
+            {
+                jogada[cooJogada[0]][cooJogada[1]] = 1;
+            }
+            validaJogada = 0;
+        }
+    }
+}
--- main.c
+++ main.c
@@ -1,7 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "funcoes.h"
 
-int main(){
+int main() {
+    int opcao;
+    int tabuleiro[7][7], jogada[7][7];
+    Navio navio[10];
+    int pronto = 0;
 
-    printf("Hello World!!!");
+    navio[0].tamanho = 4;
+    navio[1].tamanho = 3;
+    navio[2].tamanho = 3;
+    navio[3].tamanho = 2;
+    navio[4].tamanho = 2;
+    navio[5].tamanho = 2;
+    navio[6].tamanho = 1;
+    navio[7].tamanho = 1;
+    navio[8].tamanho = 1;
+    navio[9].tamanho = 1;
+
+    srand(time(NULL));
+    zeraMatriz(tabuleiro);
+    zeraMatriz(jogada);
+
+    do {
+        printf("Escolha uma opcao (1 ou 2, 0 para sair):\n");
+        printf("1. Mostrar texto 1\n");
+        printf("2. Mostrar texto 2\n");
+
+        printf("Digite sua escolha: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                draw_box_with_text(30, 10, "Bem-vindo ao Menu!"); // Moldura com texto 1
+                break;
+            case 2:
+               
+                if(pronto == 0){
+                    for (int i = 0; i < 10; i++){
+                        geraNavio(tabuleiro, &navio[i]);  
+                    }
+                    pronto = 1;
+                }
+                
+                printf("\n\nTabuleiro de navios:\n");
+                imprimeMatriz(tabuleiro, 0);
+
+                fazJogada(jogada, tabuleiro);
+                   
+                printf("\n\nTabuleiro de jogadas:\n");
+                imprimeMatriz(jogada, 1);
+                break;
+            case 0:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opcao invalida! Tente novamente.\n");
+        }
+
+        if (opcao != 0) {
+            
+            printf("\nPressione Enter para continuar...");
+            getchar(); 
+            getchar();  
+        }
+    } while (opcao != 0);
+
+
     return 0;
 }
+
+
